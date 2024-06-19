4e865eb8858c0b9f831bda0b1edf97fa63f36dca
Egg-03 <sayanbhattacharjee2001@gmail.com>
2024-06-19 10:40:31 +0530

    Update ConnectionInitializer.java

--- src/main/java/starter/ConnectionInitializer.java
+++ src/main/java/starter/ConnectionInitializer.java
@@ -6,6 +6,9 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class ConnectionInitializer {
 	
@@ -17,9 +20,15 @@ public class ConnectionInitializer {
 		String token = config.get("TOKEN");
 		
 		DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
-		builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+		builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_PRESENCES);
 		builder.setStatus(OnlineStatus.ONLINE);
 		builder.setActivity(Activity.watching("for .img your_search_term"));
+		//cache all users of the bot
+		builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+		builder.setChunkingFilter(ChunkingFilter.ALL);
+		//you will still need builder.enableCache() to cache user activities and status
+		builder.enableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.EMOJI, CacheFlag.STICKER);
+		
 		manager = builder.build();
 	}
 
