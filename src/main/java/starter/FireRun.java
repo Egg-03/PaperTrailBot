eedf813b268dffe9d5893f2135ad6f3959132a32
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-06-09 23:26:35 +0530

    Add HTTP server with ping endpoint
    Introduced an embedded HTTP server in FireRun with a '/ping' endpoint. This allows the application to respond to HTTP requests on port 8080, providing basic health check functionality.

--- src/main/java/starter/FireRun.java
+++ src/main/java/starter/FireRun.java
@@ -1,14 +1,33 @@
 package starter;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 import listeners.AuditLogListener;
 import listeners.ReactionListener;
 
 public class FireRun {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ConnectionInitializer ci = new ConnectionInitializer();
 		ci.getManager().addEventListener(new AuditLogListener());
 		ci.getManager().addEventListener(new ReactionListener());
+		
+		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/ping", new PingHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
 	}
+	
+	static class PingHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+        	 exchange.sendResponseHeaders(200, -1);
+        }
+    }
 
 }
