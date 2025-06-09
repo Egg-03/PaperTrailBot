ea3eef98e6770cfe0481dafa265a76fdea107d35
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-06-09 20:10:51 +0530

    Enhance audit log listener with embed messages
    Updated the AuditLogListener to use EmbedBuilder for creating detailed and styled audit log messages. This includes adding fields for action type, target type, and changes, along with a timestamp and footer for better readability.

--- src/main/java/listeners/AuditLogListener.java
+++ src/main/java/listeners/AuditLogListener.java
@@ -1,9 +1,12 @@
 package listeners;
 
+import java.awt.Color;
 import java.util.Map.Entry;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audit.AuditLogChange;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.guild.GuildAuditLogEntryCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -26,12 +29,34 @@ public class AuditLogListener extends ListenerAdapter{
 		 }
 		 
 		 AuditLogEntry ale = event.getEntry();
+		 EmbedBuilder eb = new EmbedBuilder();
 		 
-		 String audit = ale.getJDA().getUserById(ale.getUserIdLong()).getAsMention()+" has executed the following: \n";
-		 event.getGuild().getTextChannelById(textChannel).sendMessage(audit).queue();
-		 
+		 eb.setTitle("Audit Log Entry");
+		 eb.setDescription(ale.getJDA().getUserById(ale.getUserIdLong()).getAsMention()+" has executed the following action:");
+		 eb.setColor(Color.CYAN);
+		 eb.addField("Action Type", ale.getType().name(), true);
+		 eb.addField("Target Type", ale.getTargetType().name(), true);
+		 		 
 		 for(Entry<String, AuditLogChange> changes: ale.getChanges().entrySet()) {
-			 event.getGuild().getTextChannelById(textChannel).sendMessage(changes.getKey()+": "+ changes.getValue().getOldValue()+" to "+changes.getValue().getNewValue()).queue();
+			 String change = changes.getKey();
+			 Object oldValue = changes.getValue().getOldValue();
+			 Object newValue = changes.getValue().getNewValue();
+			 
+			 if(oldValue==null) {
+				 eb.addField(change, "to "+newValue.toString(), false);
+			 } else if(newValue==null){
+				 eb.addField(change, "from "+oldValue.toString()+" to null", false);
+			 } else {
+				 eb.addField(change, "from "+oldValue.toString()+" to "+newValue.toString(), false);
+			 }
+			 
 		 }
+		 
+		 eb.setFooter("Audit Log Entry ID: "+ale.getId());
+		 eb.setTimestamp(ale.getTimeCreated());
+		 
+		 MessageEmbed mb = eb.build();
+		 
+		 event.getGuild().getTextChannelById(textChannel).sendMessageEmbeds(mb).queue();
 	 }
 }
