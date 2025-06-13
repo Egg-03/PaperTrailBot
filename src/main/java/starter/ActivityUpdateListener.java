dac50890563109780314136bacd6f4df19fddd0e
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-06-13 22:24:29 +0530

    Update activity on guild join and leave events
    Added handlers for GuildJoinEvent and GuildLeaveEvent to update the bot's activity when it joins or leaves a guild. This ensures the displayed server count remains accurate.

--- src/main/java/starter/ActivityUpdateListener.java
+++ src/main/java/starter/ActivityUpdateListener.java
@@ -3,6 +3,8 @@ package starter;
 import org.jetbrains.annotations.NotNull;
 
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.sharding.ShardManager;
@@ -14,9 +16,23 @@ public class ActivityUpdateListener extends ListenerAdapter {
 	public ActivityUpdateListener(ShardManager manager) {
 		this.manager = manager;
 	}
-
+	
 	@Override
 	public void onReady(@NotNull ReadyEvent event) {
+		updateActivity();
+	}
+
+	@Override
+	public void onGuildJoin(@NotNull GuildJoinEvent event) {
+		updateActivity();
+	}
+
+	@Override
+	public void onGuildLeave(@NotNull GuildLeaveEvent event) {
+		updateActivity();
+	}
+
+	private void updateActivity() {
 		manager.setActivity(Activity.watching(manager.getGuilds().size() + " Servers"));
 	}
 
