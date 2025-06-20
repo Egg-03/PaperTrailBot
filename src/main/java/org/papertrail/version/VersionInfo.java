c9dd984f8278d3af744872d1daf278de505a1e1a
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-06-20 13:41:42 +0530

    Refactor setup listener and update version to 1.1.0
    Replaces SetupListener with BotSetupListener, providing improved setup instructions and updated command names. Updates BotInfoListener to show server and database locations. Bumps version to 1.1.0 and adds new fields in VersionInfo for server and database locations.

--- src/main/java/org/papertrail/version/VersionInfo.java
+++ src/main/java/org/papertrail/version/VersionInfo.java
@@ -7,7 +7,10 @@ public class VersionInfo {
 	}
 	
 	public static final String APPNAME = "PaperTrail";
-	public static final String VERSION = "v1.0.0";
+	public static final String VERSION = "v1.1.0";
 	public static final String PROJECT_LINK = "https://github.com/Egg-03/PaperTrailBot";
 	public static final String PROJECT_ISSUE_LINK="https://github.com/Egg-03/PaperTrailBot/issues";
+	
+	public static final String SERVER_LOCATION = "Render: Frankfurt(Germany, Europe)";
+	public static final String DATABASE_LOCATION = "Aiven: England(UK, Europe)";
 }
